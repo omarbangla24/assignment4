7322218ce357f427d484f508b968c15dabcbc42d
Omar Faruk <39029602+omarbangla24@users.noreply.github.com>
2020-03-14 23:35:40 +0600

    Add files via upload

---
+++ precache-manifest.1cbbebdd754659aabcab0806bd619db7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28caea76a01fdddaad513f96bb3a3d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66cb7b983d518bd4cec4",
+    "url": "/static/css/main.5a846396.chunk.css"
+  },
+  {
+    "revision": "208a98d84a569186a5db",
+    "url": "/static/js/2.1079340c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1079340c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66cb7b983d518bd4cec4",
+    "url": "/static/js/main.cc47fe7f.chunk.js"
+  },
+  {
+    "revision": "991793d1003cf134be90",
+    "url": "/static/js/runtime-main.80e4ad51.js"
+  }
+]);
